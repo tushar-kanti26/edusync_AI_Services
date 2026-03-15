29c033c61162c2df1346472fda8d25e48e9e71b4
Tushar <tusharkantisingha2601@gmail.com>
2026-03-16 00:01:22 +0530

    changed db url

--- app/Database/db.py
+++ app/Database/db.py
@@ -1,15 +1,14 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:Tks26@localhost:5432/edusync_ai"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-#engine of the databse 
 engine = create_async_engine(
     DATABASE_URL,
     echo=True
 )
 
-#local session to perform CRUD operations
 AsyncSessionLocal = sessionmaker(
     engine,
     class_=AsyncSession,
@@ -19,5 +18,3 @@ AsyncSessionLocal = sessionmaker(
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
-
-        
